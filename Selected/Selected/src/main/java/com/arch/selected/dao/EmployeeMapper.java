1eb1bfa284e193d93ad3d00124b22989448aa3ef
haiyanc123 <caihy2024@gmail.com>
2024-10-25 12:16:53 -0500

    init and add crud

---
+++ Selected/Selected/src/main/java/com/arch/selected/dao/EmployeeMapper.java
@@ -0,0 +1,16 @@
+package com.arch.selected.dao;
+
+import com.arch.selected.entity.Employee;
+import java.util.List;
+
+public interface EmployeeMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Employee record);
+
+    Employee selectByPrimaryKey(Integer id);
+
+    List<Employee> selectAll();
+
+    int updateByPrimaryKey(Employee record);
+}
---
+++ Selected/Selected/src/main/java/com/arch/selected/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.arch.selected.service;
+
+import com.arch.selected.entity.Item;
+
+/**
+ * @author: Haiyan Cai
+ * @create: 2024-10-25 11:24
+ * @description:
+ **/
+
+public interface ItemService {
+    Item findById(Integer id);
+}
