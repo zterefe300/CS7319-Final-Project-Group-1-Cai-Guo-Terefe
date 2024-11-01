8fb1b880e41ef878fea9451b6742cd2bad9978e6
haiyanc123 <caihy2024@gmail.com>
2024-11-01 11:45:21 -0500

    resolve conflict2

--- Selected/backend/src/main/java/com/selected/inventory_dashboard/controller/ItemController.java
+++ Selected/backend/src/main/java/com/selected/inventory_dashboard/controller/ItemController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/items")
 public class ItemController {
-    private final ItemService itemService;;
+    private final ItemService itemService;
 
     public ItemController(ItemService itemService) {
         this.itemService = itemService;
