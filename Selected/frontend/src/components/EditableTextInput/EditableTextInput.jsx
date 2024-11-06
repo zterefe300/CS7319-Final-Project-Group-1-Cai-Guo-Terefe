19ccadcae164f4b1eb0fd0c6c0db73ff5d64faa0
Davidg-12 <davidguo81@gmail.com>
2024-11-05 19:29:19 -0600

    Updated Profile Page

--- Selected/frontend/src/components/EditableTextInput/EditableTextInput.jsx
+++ Selected/frontend/src/components/EditableTextInput/EditableTextInput.jsx
@@ -5,17 +5,25 @@ import { TextField, Typography } from "@mui/material";
 function DisplayField({ label, value }) {
   return (
     <div style={{ display: "block" }}>
-      <Typography variant="overline" gutterBottom sx={{ display: "block" }}>
+      <Typography variant="h6" gutterBottom sx={{ display: "block" }}>
         {label}
       </Typography>
-      <Typography variant="caption" gutterBottom sx={{ display: "block" }}>
+      <Typography variant="body1" gutterBottom sx={{ display: "block" }}>
         {value}
       </Typography>
     </div>
   );
 }
 
-function TextInput({ name, id, label, onChange }) {
+function TextInput({
+  name,
+  id,
+  label,
+  onChange,
+  value,
+  type = "input",
+  error,
+}) {
   return (
     <TextField
       name={name}
@@ -24,17 +32,25 @@ function TextInput({ name, id, label, onChange }) {
       onChange={onChange}
       fullWidth
       variant="outlined"
-      // value={inputValues.adminCode}
-      type="input"
+      value={value}
+      type={type}
+      error={error}
     />
   );
 }
 
 function EditableTextInput(props) {
-  const { editMode, label, value, name, id, onChange } = props;
-  console.log("editMode", editMode);
+  const { editMode, label, value, name, id, onChange, type, error } = props;
   return editMode ? (
-    <TextInput name={name} id={id} label={label} onChange={onChange} />
+    <TextInput
+      name={name}
+      id={id}
+      label={label}
+      onChange={onChange}
+      value={value}
+      type={type}
+      error={error}
+    />
   ) : (
     <DisplayField label={label} value={value} />
   );
@@ -44,12 +60,22 @@ EditableTextInput.prototype = {
   editMode: PropTypes.bool,
   label: PropTypes.string,
   value: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  onChange: PropTypes.func,
+  type: PropTypes.string,
+  error: PropTypes.bool,
 };
 
 EditableTextInput.defaultProps = {
   editMode: false,
   label: "",
   value: "",
+  name: "",
+  id: "",
+  onChange: () => {},
+  type: "",
+  error: false,
 };
 
 export default EditableTextInput;
--- Selected/frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ Selected/frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,34 +1,111 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
+  Button,
   Card,
   CardContent,
   CardHeader,
   Container,
   Grid2,
-  IconButton,
+  Typography,
 } from "@mui/material";
-import ModeIcon from "@mui/icons-material/Mode";
 import { useParams } from "react-router-dom";
 
 import EditableTextInput from "../../components/EditableTextInput/EditableTextInput";
+import mockData from "./mockData";
 import "./ProfilePage.css";
 
 function ProfilePage() {
   const { id } = useParams();
-  const [editMode, setEditMode] = useState({
-    firstName: false,
-    lastName: false,
+  const [editMode, setEditMode] = useState(false);
+  const [editInputs, setEditInputs] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   });
 
-  const handleEditButtonCLick = (name) => {
-    setEditMode((prevState) => ({
+  const [errors, setErrors] = useState({
+    firstName: false,
+    lastName: false,
+    userName: false,
+    email: false,
+    password: false,
+    confirmPassword: false,
+  });
+
+  useEffect(() => {
+    setEditInputs((prevState) => ({
       ...prevState,
-      [name]: !prevState.name,
+      ...mockData,
+    }));
+  }, []);
+
+  const isValidInput = () => {
+    let errorField = {};
+    const {
+      firstName = "",
+      lastName = "",
+      userName = "",
+      email = "",
+    } = editInputs;
+
+    const isValidEmail = email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+
+    firstName
+      ? (errorField = { ...errorField, firstName: false })
+      : (errorField = { ...errorField, firstName: true });
+    lastName
+      ? (errorField = { ...errorField, lastName: false })
+      : (errorField = { ...errorField, lastName: true });
+    userName
+      ? (errorField = { ...errorField, userName: false })
+      : (errorField = { ...errorField, userName: true });
+    isValidEmail
+      ? (errorField = { ...errorField, email: false })
+      : (errorField = { ...errorField, email: "Enter a correct email" });
+
+    setErrors(errorField);
+    return Boolean(firstName && lastName && userName && isValidEmail);
+  };
+
+  const handleEditButtonCLick = (name) => {
+    setEditMode((prevState) => !prevState);
+  };
+
+  const handleCancelButtonClick = () => {
+    // TODO: Reset the data to original api call data
+    setEditInputs({
+      ...mockData,
+      password: "",
+      confirmPassword: "",
+    });
+    setEditMode((prevState) => !prevState);
+  };
+
+  const handleSaveButtonClick = () => {
+    // TODO: Connect the save button to API calls to save the data, and the fetch the new data.
+    const isValid = isValidInput();
+    if (!isValid) {
+      alert("Something wrong");
+    } else {
+      setEditMode((prevState) => !prevState);
+    }
+  };
+
+  const handleInputChanges = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setEditInputs((prevState) => ({
+      ...prevState,
+      [name]: value,
     }));
   };
 
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="md">
       <Card variant="outlined" sx={{ backgroundColor: "#fafafa" }}>
         <CardHeader
           title="Profile Page"
@@ -39,41 +116,95 @@ function ProfilePage() {
         <CardContent>
           <Grid2 container spacing={2}>
             <Grid2 size={12}>
-              <div className="ProfilePage__div__flex">
-                <EditableTextInput
-                  editMode={editMode.firstName}
-                  label="First Name"
-                  value="firstName"
-                  name="firstName"
-                  id="firstName"
-                  onChange={() => {}}
-                />
-                <IconButton
-                  onClick={() => handleEditButtonCLick("firstName")}
-                  sx={{ p: 0 }}
-                  color="inherit"
-                >
-                  <ModeIcon />
-                </IconButton>
-              </div>
+              <EditableTextInput
+                editMode={editMode}
+                label="First Name"
+                value={editInputs.firstName}
+                name="firstName"
+                id="firstName"
+                onChange={handleInputChanges}
+                error={errors.firstName}
+              />
             </Grid2>
             <Grid2 size={12}>
-              <div className="ProfilePage__div__flex">
+              <EditableTextInput
+                editMode={editMode}
+                label="Last Name"
+                value={editInputs.lastName}
+                name="lastName"
+                id="lastName"
+                onChange={handleInputChanges}
+                error={errors.lastName}
+              />
+            </Grid2>
+            <Grid2 size={12}>
+              <EditableTextInput
+                editMode={editMode}
+                label="User Name"
+                value={editInputs.userName}
+                name="userName"
+                id="userName"
+                onChange={handleInputChanges}
+                error={errors.userName}
+              />
+            </Grid2>
+            <Grid2 size={12}>
+              <EditableTextInput
+                editMode={editMode}
+                label="Email"
+                value={editInputs.email}
+                name="email"
+                id="email"
+                onChange={handleInputChanges}
+                error={errors.email}
+              />
+            </Grid2>
+            <Grid2 size={12}>
+              <EditableTextInput
+                editMode={editMode}
+                label="Password"
+                value={editInputs.password}
+                name="password"
+                id="password"
+                onChange={handleInputChanges}
+                type="password"
+              />
+            </Grid2>
+            {editMode && (
+              <Grid2 size={12}>
                 <EditableTextInput
-                  editMode={editMode.lastName}
-                  label="Last Name"
-                  value="lastName"
-                  name="lastName"
-                  id="lastName"
-                  onChange={() => {}}
+                  editMode={editMode}
+                  label="Confirm Password"
+                  value={editInputs.confirmPassword}
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  onChange={handleInputChanges}
+                  type="password"
                 />
-                <IconButton
-                  onClick={() => handleEditButtonCLick("lastName")}
-                  sx={{ p: 0 }}
-                  color="inherit"
-                >
-                  <ModeIcon />
-                </IconButton>
+              </Grid2>
+            )}
+            {errors.email && (
+              <Grid2 size={12}>
+                <Typography color="error">{errors.email}</Typography>
+              </Grid2>
+            )}
+            <Grid2 size={12}>
+              <div className="ProfilePage__div__flex">
+                {!editMode && (
+                  <Button variant="contained" onClick={handleEditButtonCLick}>
+                    Edit
+                  </Button>
+                )}
+                {editMode && (
+                  <Button variant="contained" onClick={handleSaveButtonClick}>
+                    Save Changes
+                  </Button>
+                )}
+                {editMode && (
+                  <Button variant="text" onClick={handleCancelButtonClick}>
+                    Cancel
+                  </Button>
+                )}
               </div>
             </Grid2>
           </Grid2>
