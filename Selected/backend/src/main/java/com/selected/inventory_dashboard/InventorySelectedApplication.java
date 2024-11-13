43841f3997b52c9492b047f66d5e17c1774096af
Zterefe@smu.edu <cico7899@gmail.com>
2024-11-13 11:55:36 -0600

    Task-19: Refactor item service impl

--- Selected/backend/src/main/java/com/selected/inventory_dashboard/InventorySelectedApplication.java
+++ Selected/backend/src/main/java/com/selected/inventory_dashboard/InventorySelectedApplication.java
@@ -13,5 +13,4 @@ public class InventorySelectedApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventorySelectedApplication.class, args);
     }
-
 }
--- Selected/backend/src/main/java/com/selected/inventory_dashboard/dtovo/req/ItemRequest.java
+++ Selected/backend/src/main/java/com/selected/inventory_dashboard/dtovo/req/ItemRequest.java
@@ -2,5 +2,5 @@ package com.selected.inventory_dashboard.dtovo.req;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public record ItemRequest(String name, String detail, Integer quantity, MultipartFile picturesStream, Integer quantityAlarmThreshold,
+public record ItemRequest(String name, String detail, Integer quantity, MultipartFile pictureStream, Integer quantityAlarmThreshold,
                           Integer quantityReorderThreshold, Integer vendorId) { }
