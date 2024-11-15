a9dd1f64ff22b2c50739117a66089f9ac4556320
Zterefe@smu.edu <cico7899@gmail.com>
2024-11-14 19:19:54 -0600

    Task-6: Fix CORS problem

--- Selected/backend/src/main/java/com/selected/inventory_dashboard/config/WebConfig.java
+++ Selected/backend/src/main/java/com/selected/inventory_dashboard/config/WebConfig.java
@@ -1,14 +1,26 @@
 package com.selected.inventory_dashboard.config;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@ControllerAdvice
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigins;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**").addResourceLocations("file:uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins.split(","))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*").allowCredentials(true);
     }
 }
